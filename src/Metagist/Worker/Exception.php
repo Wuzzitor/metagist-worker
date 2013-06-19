2e11948638a37f620e7f83feb46c67038ae1e1e2
Daniel Pozzi <bonndan76@googlemail.com>
2013-06-19 18:16:50 +0200

    metagist worker exception

---
+++ src/Metagist/Worker/Exception.php
@@ -0,0 +1,12 @@
+<?php
+namespace Metagist\Worker;
+
+/**
+ * Metagist Worker exception.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class Exception extends \Exception
+{
+    
+}
