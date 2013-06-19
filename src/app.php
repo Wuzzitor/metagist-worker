11d6b2eeba7fa33f35866dd0f82f9b2dae0513e2
Daniel Pozzi <bonndan76@googlemail.com>
2013-06-19 16:53:17 +0200

    app bootstrapping script

---
+++ src/app.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Metagist Worker application bootstrapping
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+
+/*
+ * Monolog
+ */
+use Silex\Provider\MonologServiceProvider;
+$app->register(new MonologServiceProvider(), array(
+    'monolog.logfile' => __DIR__ . '/../log/app.log',
+    'monolog.name' => 'app',
+    'monolog.level' => 300 // = Logger::WARNING
+));
+
+/**
+ * Enable the metagist api
+ */
+$app->register(new Metagist\Api\ServiceProvider());
+
+/*
+ * The api controller registers itself
+ */
+new Metagist\Worker\ApiController($app);
+
+return $app;
