297cc744b5433cd538804e9855a9600d835d00c1
Daniel Pozzi <bonndan76@googlemail.com>
2013-06-19 22:07:27 +0200

    api controller

---
+++ src/Metagist/Worker/ApiController.php
@@ -0,0 +1,92 @@
+<?php
+namespace Metagist\Worker;
+
+/**
+ * Api Controller.
+ * 
+ * Receives "scan" requests and forwards them as gearman job.
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class ApiController implements \Metagist\Api\WorkerInterface
+{
+    /**
+     * the application instance
+     * 
+     * @var \Metagist\Worker\Application 
+     */
+    protected $application;
+
+    /**
+     * Constructor.
+     * 
+     * @param \Metagist\Worker\Application  $app
+     */
+    public function __construct(Application $app)
+    {
+        $this->application = $app;
+        $this->initRoutes();
+    }
+    
+    /**
+     * routes.
+     * @var array 
+     */
+    protected $routes = array(
+        'index'     => array('match' => '/', 'method' => 'index'),
+        'scan'      => array('match' => '/api/scan/{author}/{name}', 'method' => 'scan'),
+    );
+    
+    /**
+     * Setup of the api routes.
+     * 
+     * 
+     */
+    protected function initRoutes()
+    {
+        foreach ($this->routes as $name => $data) {
+            $this->application
+                ->match($data['match'], array($this, $data['method']))
+                ->bind($name);
+        }
+    }
+
+    /**
+     * Index: returns nothing useful.
+     * 
+     * @return string
+     */
+    public function index()
+    {
+        $this->application->getServerClient();
+        return $this->application->json("Metagist Worker");
+    }
+    
+    /**
+     * Returns the package content as json.
+     * 
+     * @param string $author
+     * @param string $name
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
+     */
+    public function scan($author, $name)
+    {
+        $message = \Symfony\Component\HttpFoundation\Request::createFromGlobals();
+        try {
+            $consumerKey = $this->application->getApi()->validateRequest($message->__toString());
+        } catch (\Metagist\Api\Exception $exception) {
+            return $this->application->json($exception->getMessage(), 403);
+        }
+        
+        $this->application->getLogger()->info(
+            'Received scan request from ' . $consumerKey. ' to scan ' . $author . '/' . $name
+        );
+        
+        try {
+            $this->application->requestScan($author . '/' . $name);
+            return $this->application->json('Queued job to scan package ' . $author . '/' . $name);
+        } catch (\Metagist\Worker\Exception $exception) {
+            return $this->application->json($exception->getMessage(), 500);
+        } 
+    }
+}
---
+++ tests/src/Metagist/Worker/ApiControllerTest.php
@@ -0,0 +1,105 @@
+<?php
+namespace Metagist\Worker;
+
+require_once __DIR__ . '/bootstrap.php';
+
+/**
+ * Tests the metagist worker api controller
+ * 
+ * @author Daniel Pozzi <bonndan76@googlemail.com>
+ */
+class ApiControllerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * app 
+     * @var Application
+     */
+    private $app;
+    
+    /**
+     * app 
+     * @var Application
+     */
+    private $apiMock;
+    
+    /**
+     * system under test
+     * @var ApiController
+     */
+    private $controller;
+    
+    /**
+     * gearman client mock
+     * 
+     * @var \Gearman
+     */
+    private $gearman;
+    
+    /**
+     * Test setup.
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->app = new \Metagist\Worker\Application();
+        $this->app['monolog'] = $this->getMock("\Psr\Log\LoggerInterface");
+        $this->apiMock        = $this->getMock("\Metagist\Api\ServiceProvider");
+        $this->app[\Metagist\Api\ServiceProvider::API] = $this->apiMock;
+        
+        /*
+         * gearman mocking
+         */
+        if (!defined('GEARMAN_SUCCESS')) {
+            define('GEARMAN_SUCCESS', 'GEARMAN_SUCCESS');
+        }
+        $this->gearman = $this->getMock("\GearmanClient", array('doBackground', 'returnCode'));
+        $this->app->setGearmanClient($this->gearman);
+        
+        $this->controller = new ApiController($this->app);
+    }
+    
+    /**
+     * Ensures the scan request is queued.
+     */
+    public function testScan()
+    {
+        $this->gearman->expects($this->once())
+            ->method('returnCode')
+            ->will($this->returnValue(GEARMAN_SUCCESS));
+        
+        $response = $this->controller->scan('test', 'test');
+        $this->assertInstanceOf("\Symfony\Component\HttpFoundation\JsonResponse", $response);
+        
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertContains('Queued job to scan package', $response->getContent());
+    }
+    
+    /**
+     * Ensures the request is validated using oauth
+     */
+    public function testRequestValidationFails()
+    {
+        $this->gearman->expects($this->never())
+            ->method('doBackground');
+        $this->apiMock->expects($this->once())
+            ->method('validateRequest')
+            ->will($this->throwException(new \Metagist\Api\Exception('test')));
+        
+        $response = $this->controller->scan('test', 'test');
+        $this->assertEquals(403, $response->getStatusCode());
+    }
+    
+    /**
+     * 
+     */
+    public function testGearmanException()
+    {
+        $this->gearman->expects($this->once())
+            ->method('returnCode')
+            ->will($this->returnValue(FALSE));
+        
+        $response = $this->controller->scan('test', 'test');
+        $this->assertEquals(500, $response->getStatusCode());
+    }
+}
