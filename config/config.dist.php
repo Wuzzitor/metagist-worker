0278107372173a97c4de9d3fde0c90b337f5ca96
Daniel Pozzi <bonndan76@googlemail.com>
2013-07-12 20:52:41 +0200

    sample config

--- config/config.dist.php
+++ config/config.dist.php
@@ -19,12 +19,27 @@ $app[\Metagist\Api\ServiceProvider::APP_SERVER_CONFIG] = array(
 );
 $app[\Metagist\Api\ServiceProvider::APP_SERVICES] = __DIR__ . '/../config/services.json';
 
+/**
+ * Which scanners to use for info retrieval.
+ */
 $app[\Metagist\Worker\Scanner\PackageScanner::ENABLED_SCANNERS] = array(
     "\Metagist\Worker\Scanner\Packagist",
     "\Metagist\Worker\Scanner\GitHub",
 );
 
+/**
+ * Github api config.
+ */
 $app[\Metagist\Worker\Scanner\GitHub::GITHUB_CLIENT_CONFIG] = array(
     "client_id" => 'your-api-id',
     "client_secret" => 'your-api-secret'
+);
+
+
+/**
+ * Packagist feed consumption configuration
+ */
+$app[\Metagist\Worker\Application::FEED_CONFIG] = array(
+    'cache_dir' => dirname(__DIR__) . '/cache',
+    'feed_url'  => 'https://packagist.org/feeds/releases.rss',
 );
